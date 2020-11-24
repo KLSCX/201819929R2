b59ba32538d1bc967fbb37fbdb9abba41c52a5d2
KLSCX <2576723093@qq.com>
2020-11-24 20:57:46 +0800

    系统二

--- SY1/v1.java
+++ SY1/v1.java
@@ -1,34 +1,42 @@
 package SY1;
 import java.util.Random;
-public class PracticeSystem1{
+public class PracticeSystem2{
 
     public static void main(String[] args) {
-        printExercise(Equation());
+        int[][] Exercise;
+        Exercise = Equation();
+        System.out.println("打印习题");
+        printExercise( Exercise);
     }
     //产生算式
     public static int[] formula(){
-
         Random random = new Random();
         int[] Equation = new int[3];
-
-        int leftOperand = operand();
-        Equation[0] = leftOperand;
-
-        int rightOperand = operand();
-        Equation[1] = rightOperand;
-
-        int operator = random.nextInt(2);
-        Equation[2] = operator;
-
+        Equation[2] = random.nextInt(2);
+        if(Equation[2]==1){
+            plus(Equation);
+        }else {
+            minus(Equation);
+        }
         return Equation;
+    }
+    //加法运算
+    public static int[] plus(int a[]){
+            a[0] = operand();
+            a[1] = operand();
+        return a;
+    }
+    //减法运算
+    public static int[] minus(int a[]){
+            a[0] = operand();
+            a[1] = operand();
+        return a;
     }
     //生成习题
     public static int[][] Equation(){
         int[][] Exercise=new int[50][3];
         for (int i = 0; i < 50; i++) {
-            Exercise[i][0] = formula()[0];
-            Exercise[i][1] = formula()[1];
-            Exercise[i][2] = formula()[2];
+            Exercise[i] = formula();
         }
         return Exercise;
     }
@@ -38,25 +46,14 @@ public class PracticeSystem1{
         int operand = random.nextInt(101);
         return operand;
     }
-    //加法运算
-    public static int plus(int a[]){
-        int value = a[0] + a[1];
-        return value;
-    }
-    //减法运算
-    public static int minus(int a[]){
-        int value = a[0] - a[1];
-        return value;
-    }
     //打印习题
     public static void printExercise(int a[][]){
-       for (int i = 0; i < 50; i++) {
-           if (a[i][2]==1){
-            System.out.printf("%3d + %3d =           ", a[i][0], a[i][1]);
-        }else {
-            System.out.printf("%3d - %3d =           ", a[i][0], a[i][1]);
-        }
-
+        for (int i = 0; i < 50; i++){
+            if (a[i][2]==1){
+                System.out.printf("%-3d + %-3d =            ", a[i][0], a[i][1]);
+            }else {
+                System.out.printf("%-3d - %-3d =            ", a[i][0], a[i][1]);
+            }
             if ((i+1) % 5 == 0) {
                 System.out.println();
             }
