d11c07860965e8d3cb866f3fd1c0d6397182731a
KLSCX <2576723093@qq.com>
2020-11-24 21:14:00 +0800

    系统1.3

--- SY1/v1.java
+++ SY1/v1.java
@@ -1,15 +1,18 @@
 package SY1;
 import java.util.Random;
-public class PracticeSystem2{
+public class PracticeSystem3 {
 
     public static void main(String[] args) {
         int[][] Exercise;
         Exercise = Equation();
         System.out.println("打印习题");
         printExercise( Exercise);
+        System.out.println("打印答案");
+        printCalculate(Exercise);
     }
     //产生算式
     public static int[] formula(){
+
         Random random = new Random();
         int[] Equation = new int[3];
         Equation[2] = random.nextInt(2);
@@ -20,16 +23,27 @@ public class PracticeSystem2{
         }
         return Equation;
     }
+    //约束条件检测函数
+    public static boolean check(int anInteger,int low,int high){
+        return anInteger >= low && anInteger <= high ;
+    }
     //加法运算
     public static int[] plus(int a[]){
+        do {
             a[0] = operand();
             a[1] = operand();
+        }while (!check(a[0],0,100)||!check(a[1],0,100) ||!check(a[0]+a[1],0,100));
+
         return a;
     }
     //减法运算
     public static int[] minus(int a[]){
+        do {
             a[0] = operand();
+
             a[1] = operand();
+
+        }while (!check(a[0],0,100)||!check(a[1],0,100) ||!check(a[0]-a[1],0,100));
         return a;
     }
     //生成习题
@@ -46,14 +60,29 @@ public class PracticeSystem2{
         int operand = random.nextInt(101);
         return operand;
     }
+
     //打印习题
     public static void printExercise(int a[][]){
-        for (int i = 0; i < 50; i++){
-            if (a[i][2]==1){
-                System.out.printf("%-3d + %-3d =            ", a[i][0], a[i][1]);
-            }else {
-                System.out.printf("%-3d - %-3d =            ", a[i][0], a[i][1]);
+       for (int i = 0; i < 50; i++) {
+           if (a[i][2]==1){
+            System.out.printf("%-3d + %-3d =            ", a[i][0], a[i][1]);
+        }else {
+            System.out.printf("%-3d - %-3d =            ", a[i][0], a[i][1]);
+        }
+            if ((i+1) % 5 == 0) {
+                System.out.println();
             }
+        }
+    }
+    //打印答案
+    public static void printCalculate(int a[][]){
+        for (int i = 0; i < 50; i++) {
+            if (a[i][2]==1){
+                System.out.printf("%-3d + %-3d =    %-3d     ", a[i][0], a[i][1],a[i][0]+a[i][1]);
+            }else {
+                System.out.printf("%-3d - %-3d =    %-3d     ", a[i][0], a[i][1],a[i][0]-a[i][1]);
+            }
+
             if ((i+1) % 5 == 0) {
                 System.out.println();
             }
